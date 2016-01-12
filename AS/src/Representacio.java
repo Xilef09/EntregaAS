bba7e0ba9f3bfd50cb864dcb53b048fbb8b877ac
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-12 21:06:11 +0100

    .java

--- AS/src/Representacio.java
+++ AS/src/Representacio.java
@@ -2,15 +2,28 @@ import java.util.Date;
 
 public class Representacio {
 	
+	public enum TipusSessio {
+	    matí, tarda, nit
+	}
+	
+	private TipusSessio sessio;
+	
+	private String titolEspectacle;
+	
+	private String nomLocal;
+	
 	private Float preu;
 	private Date data;
 	private Integer nombreSeientsLliures;
 	
-	public Representacio(Float preu, Date data, Integer nombreSeientsLliures) {
+	public Representacio(Float preu, Date data, Integer nombreSeientsLliures, TipusSessio sessio, String titolEspectacle, String nomLocal) {
 		super();
 		this.preu = preu;
 		this.data = data;
 		this.nombreSeientsLliures = nombreSeientsLliures;		
+		this.sessio = sessio;		
+		this.titolEspectacle = titolEspectacle;		
+		this.nomLocal = nomLocal;			
 	}
 
 	public Float getPreu() {
@@ -35,6 +48,30 @@ public class Representacio {
 
 	public void setNombreSeientsLliures(Integer nombreSeientsLliures) {
 		this.nombreSeientsLliures = nombreSeientsLliures;
+	}
+
+	public String getNomLocal() {
+		return nomLocal;
+	}
+
+	public void setNomLocal(String nomLocal) {
+		this.nomLocal = nomLocal;
+	}
+
+	public String getTitolEspectacle() {
+		return titolEspectacle;
+	}
+
+	public void setTitolEspectacle(String titolEspectacle) {
+		this.titolEspectacle = titolEspectacle;
+	}
+
+	public TipusSessio getSessio() {
+		return sessio;
+	}
+
+	public void setSessio(TipusSessio sessio) {
+		this.sessio = sessio;
 	}
 
 }
