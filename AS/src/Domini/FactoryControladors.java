1156472eabb518218775d20ca1feae0a58d3e320
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-13 20:03:02 +0100

    molta trama, per aqui i per alla

--- AS/src/Domini/FactoryControladors.java
+++ AS/src/Domini/FactoryControladors.java
@@ -3,6 +3,13 @@ package Domini;
 public class FactoryControladors {
 
 	private static FactoryControladors instance = null;
+	
+	private ControladorRepresentacio ctrRepresentacio = new ControladorRepresentacio();
+	private ControladorEspectacle ctrEspectacle = new ControladorEspectacle();
+	private ControladorEntrada ctrEntrada = new ControladorEntrada();
+	private ControladorSeientsEnRepresentacio ctrSeientsEnRepresentacio = new ControladorSeientsEnRepresentacio();
+	private CtrlConsultaOcupacio ctrlConsultaOcupacio = new CtrlConsultaOcupacio();
+	private CtrlConsultaRepresentacions ctrlConsultaRepresentacions = new CtrlConsultaRepresentacions();
 
 	protected FactoryControladors() {}
 
@@ -13,4 +20,27 @@ public class FactoryControladors {
         return instance;
     }
 
+	public ControladorRepresentacio getCtrRepresentacio() {
+		return ctrRepresentacio;
+	}
+
+	public ControladorEspectacle getCtrEspectacle() {
+		return ctrEspectacle;
+	}
+
+	public ControladorEntrada getCtrEntrada() {
+		return ctrEntrada;
+	}
+
+	public CtrlConsultaRepresentacions getCtrlConsultaRepresentacions() {
+		return ctrlConsultaRepresentacions;
+	}
+
+	public ControladorSeientsEnRepresentacio getCtrSeientsEnRepresentacio() {
+		return ctrSeientsEnRepresentacio;
+	}
+
+	public CtrlConsultaOcupacio getCtrlConsultaOcupacio() {
+		return ctrlConsultaOcupacio;
+	}	
 }
