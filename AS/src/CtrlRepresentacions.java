d8d4306b27a80b6df6acbd33f3d05306ca385688
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-13 13:41:39 +0100

    afegides clase CtrlRepresentacions

---
+++ AS/src/CtrlRepresentacions.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class CtrlRepresentacions {
+
+	private DataFactory dataFactory = null;
+	
+	public CtrlRepresentacions() {
+		// TODO Auto-generated constructor stub
+		dataFactory = DataFactory.getInstance();
+	}
+	
+	public ArrayList<Representacio> consultaRepresentacions(String titolEspectacle, String data){
+		
+	}
+	
+}
