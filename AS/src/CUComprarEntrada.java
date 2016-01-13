1139e2789538c15dac5edc89033d5615577d0898
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-13 11:59:28 +0100

    anadido cucomprarEntr

---
+++ AS/src/CUComprarEntrada.java
@@ -0,0 +1,18 @@
+
+public class CUComprarEntrada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javax.swing.SwingUtilities.invokeLater (new Runnable() {
+			public void run() {
+				/*
+				Ejemplo:
+				CPJugarPartida ctrlJugar = new CPJugarPartida();
+				ctrlJugar.inicializarLogin();
+				
+				Ejecutar funcion que crea vista e inicia el CUComprarEntrada
+				*/
+			}
+		});
+	}
+}
