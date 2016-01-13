386da6c30beca4cdfcc8fc4e1ab1aafae9a9cf67
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2016-01-13 18:57:32 +0100

    half interficies

--- AS/src/ControladorSeients.java
+++ AS/src/ControladorSeients.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 
 public interface ControladorSeients {
-
+	public Seient get(String nomLocal, int fila, int columna);
+	public Boolean exists(String nomLocal, int fila, int columna);
+	public ArrayList<Seient> all ();
 }
