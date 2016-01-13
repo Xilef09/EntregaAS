224b533eb5481a850fc0c96c0ed2bde36c20f30a
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2016-01-13 18:23:12 +0100

    vec

--- AS/src/ControladorEntradaDB.java
+++ AS/src/ControladorEntradaDB.java
@@ -1,4 +1,4 @@
 
-public class ControladorEntradaDB {
+public class ControladorEntradaDB implements ControladorEntrada{
 
 }
--- AS/src/ControladorRepresentacioDB.java
+++ AS/src/ControladorRepresentacioDB.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ControladorRepresentacioDB {
+public class ControladorRepresentacioDB implements ControladorRepresentacio{
 
 	private CtrlDataFactory dataFactory = null;
 	
