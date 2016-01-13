29100d87820375d8fe4e565698a389ca5c96e918
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-01-13 11:07:36 +0100

    All hibernate files (incomplete files)

--- AS/src/Main2.java
+++ AS/src/Main2.java
@@ -1,4 +1,15 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 
 public class Main2 {
 
+	
+	public static void main(String[] args) {
+		
+		//SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		//Session session = factory.openSession();
+		
+	}
 }
