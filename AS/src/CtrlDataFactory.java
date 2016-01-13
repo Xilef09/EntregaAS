edb78ab74be11ba6f4f85ebe4a9bb62a53f15956
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2016-01-13 18:02:55 +0100

    creando nuevos controladores

--- AS/src/CtrlDataFactory.java
+++ AS/src/CtrlDataFactory.java
@@ -1,8 +1,14 @@
 
 public class CtrlDataFactory {
 	private static CtrlDataFactory instance= null;
-	private ControladorRepresentacio = new ControladorRepresentacio();
-	
+	private ControladorRepresentacio controladorRepresentacio = new ControladorRepresentacio();
+	private ControladorEspectacle controladorEspectacle = new ControladorEspectacle();
+	private ControladorSessio controladorSessio = new ControladorSessio();
+	private ControladorSeients controladorSeients = new ControladorSeients();
+	private ControladorSeientsEnRepresentacio controladorSeientsEnRepresentacio = new ControladorSeientsEnRepresentacio();
+	private ControladorLocal controladorLocal = new ControladorLocal();
+	private ControladorEstrena controladorEstrena = new ControladorEstrena();
+	private ControladorEntrada controladorEntrada = new ControladorEntrada();
 	protected CtrlDataFactory(){
 	}
 	
