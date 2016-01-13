8ffd3c9db6eeb1e5fe2df331f564766c69a74cca
Guillem Bonafonte <guillem.bonafonte@est.fib.upc.edu>
2016-01-13 20:18:05 +0100

    comprarentradactrlus

--- AS/src/Domini/CtrlComprarEntrada.java
+++ AS/src/Domini/CtrlComprarEntrada.java
@@ -1,9 +1,38 @@
 package Domini;
 
-public class CtrlComprarEntrada {
+import java.util.ArrayList;
 
-	public CtrlComprarEntrada() {
-		// TODO Auto-generated constructor stub
+import org.javatuples.*;
+
+
+
+public class CtrlComprarEntrada {
+	private String titol;
+	private String date;
+	private String nomLocal;
+	private String sessio;
+	private Integer nombEspectadors;
+	private ArrayList<Pair<Integer,Integer>> seients;
+	private Float preuTotal;
+	
+	public ArrayList<Quintet<String,String,Integer,Boolean,Float>> obteRepresentacions(String titol, String date){
+		return null; //TO DO
 	}
+	public ArrayList<Pair> obteOcupacio(String nomLocal, String sessio, Integer nombEspectadors){
+		return null; //TO DO 
+	}
+	public Pair<Float,ArrayList<String>> seleccionarSeients (ArrayList<Pair> seients){
+		return null; //TO DO
+	}
+	public float obtePreuMoneda(String moneda){
+		return (Float) null; //TO DO 
+	}
+	public void pagament (String dni, Integer codiB, String numCompte){
+		//TO DO
+	}
+	public ArrayList<String> obteEspectacles(){
+		return null; //TO DO
+	}
+	
 
 }
