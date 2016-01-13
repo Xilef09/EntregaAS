89690ed3f1a22b5e4615099840af7b6f5fc52211
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-13 18:43:12 +0100

    ctrrep

--- AS/src/ControladorRepresentacio.java
+++ AS/src/ControladorRepresentacio.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 
 public interface ControladorRepresentacio {
 
+	public Representacio get(String sessio, String nomLocal) throws Exception;
+	
+	public Boolean exists(String sessio, String nomLocal) throws Exception;
+	
+	public ArrayList<Representacio> all()throws Exception;
 }
