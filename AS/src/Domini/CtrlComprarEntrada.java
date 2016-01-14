566db83e2e46d508167a11cc464601a362493b1d
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-14 14:37:51 +0100

    molta trama, per aqui i per alla

--- AS/src/Domini/CtrlComprarEntrada.java
+++ AS/src/Domini/CtrlComprarEntrada.java
@@ -49,5 +49,6 @@ public class CtrlComprarEntrada {
 		return cr.consultaEspectacles();
 	}
 	
+	
 
 }
---
+++ AS/src/Domini/CurrentConvertorService.java
@@ -0,0 +1,10 @@
+package Domini;
+
+public class CurrentConvertorService {
+
+	public CurrentConvertorService() {}
+	
+	public float conversionRate(String divisa, String moneda){
+		return 0; //TO DO
+	}
+}
