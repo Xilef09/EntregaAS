e3aa9f925d172ecadf54bb6536ee0fdb606bef9e
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-01-14 13:02:34 +0100

    molta trama, per aqui i per alla

--- AS/src/Domini/CtrlConsultaOcupacio.java
+++ AS/src/Domini/CtrlConsultaOcupacio.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 import org.javatuples.*;
 
 public class CtrlConsultaOcupacio {
+	
+	private FactoryControladors myFactory = FactoryControladors.getInstance();
 
-	public ArrayList<Pair<Integer,Integer>> consultaOcupacio(String nomLocal, String sessio, Integer nombEspectadors){
-		return null; //TO DO
+	public ArrayList<Pair<Integer,Integer>> consultaOcupacio(String nomLocal, String sessio, Integer nombEspectadors) throws Exception{
+		ControladorRepresentacio ctrConsultaRep = myFactory.getCtrRepresentacio();
+		Representacio r = ctrConsultaRep.getRepresentacio(nomLocal, sessio);
+		r.hiHaEspai(nombEspectadors);
+		return r.consultaOcupacio("lliure");
 	}
+	
 	public ArrayList<Septet<String,String,String,String,Integer,Boolean,Float>> totesRepresentacions(){
 		return null; //TO DO 
 	}
